2527b5de7df2e57739ca61b03546a59cb42e7e3b
flames of love <mechanicofthesequence@gmail.com>
2014-01-01 01:27:40 +0100

    almost there

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   Conductor: require('./lib/conductor'),
   Wisdom: require('./lib/wisdom'),
   Insight: require('./lib/insight'),
+  Ambition: require('./lib/consciousness/ambition'),
   Philosopher: require('./lib/philosopher')
 }
